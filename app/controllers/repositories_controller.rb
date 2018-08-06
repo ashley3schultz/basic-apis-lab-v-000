e69863eceec9b25401cdc39910f59f43d6e57924
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-06 22:40:12 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,5 +10,8 @@ class RepositoriesController < ApplicationController
       req.params["client_id"] = 'd0c0327d107d671d59f5'
       req.params["client_secret"] = 'e8ca50d385e8c03f74c8deb75de3667909b058fa'
     end
+    body = JSON.parse(resp.body)
+    @results = body["items"]
+    render :search
   end
 end
